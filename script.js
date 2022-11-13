e0f48478e994eac97e8d8579ca7768c9dc52cb41
AlexeySitnikov <alexey.sitnikov@bk.ru>
2022-11-13 23:25:26 +0300

    the requiest for all cats and show its names

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const getAllCatsURL =  "http://sb-cats.herokuapp.com/api/2/alexeysitnikov/show";
 const $buttonGetAllCats = document.querySelector('[data-getAllCats]');
 const $divContainer = document.querySelector('[data-container]');
 
+
 const getAllCats = ()=>{
   fetch(getAllCatsURL)
   .then((response)=>response.json())
-  .then((json)=>{console.log(json)});
+  .then((json)=>{
+    const h1 = json.data.map((el)=>(`<p>${el.name}</p>`)).join("");
+    $divContainer.insertAdjacentHTML('beforeend',h1);
+  });
 }
 
 
