33863051578925616ff198ce81a60b7a8f831e0b
AlexeySitnikov <alexey.sitnikov@bk.ru>
2022-11-15 00:26:59 +0300

    trying to make the buttons work

--- script.js
+++ script.js
@@ -4,17 +4,32 @@ const $buttonGetAllCats = document.querySelector('[data-getAllCats]');
 const $divContainer = document.querySelector('[data-container]');
 
 
+const cardsHTML = (card)=>{
+  return `<div class="card mx-6" style="width: 18rem;">
+  <img src="${card.img_link}" class="card-img-top" alt="${card.name}">
+  <div class="card-body">
+    <h5 class="card-title">${card.name}</h5>
+    <p class="card-text">${card.description}</p>
+  </div>
+  <button data-action="show" type="button" class="btn btn-primary">Primary</button>
+  <button data-action="delete" type="button" class="btn btn-danger">Danger</button>
+</div>`;  
+}
+
 const getAllCats = ()=>{
   fetch(getAllCatsURL)
   .then((response)=>response.json())
   .then((json)=>{
-    const h1 = json.data.map((el)=>(`<p>${el.name}</p>`)).join("");
-    $divContainer.insertAdjacentHTML('beforeend',h1);
+    const cardCat = json.data.map((el)=>(cardsHTML(el))).join("");
+    $divContainer.insertAdjacentHTML('beforeend', cardCat);
   });
 }
 
 
 $buttonGetAllCats.addEventListener("click", getAllCats);
 
-
+$divContainer.addEventListener("click",(e)=>{
+  //if (e.target.action === "show")
+  console.log( e.target.closest("[data-action]"));
+})
 
