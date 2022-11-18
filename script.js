c30caead965f9281161f844a3e40a45f597059f3
AlexeySitnikov <alexey.sitnikov@bk.ru>
2022-11-19 00:25:49 +0300

    show button works

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const getAllCatsURL =  "http://sb-cats.herokuapp.com/api/2/alexeysitnikov/show";
 const deleteCardById = " http://sb-cats.herokuapp.com/api/2/alexeysitnikov/delete/";
-const addCatURL = "http://sb-cats.herokuapp.com/api/2/alexeysitnikov/add"
+const addCatURL = "http://sb-cats.herokuapp.com/api/2/alexeysitnikov/add";
+const getOneCat = "http://sb-cats.herokuapp.com/api/2/alexeysitnikov/show/";
 
-const $buttonGetAllCats = document.querySelector('[data-getAllCats]');
+// const $buttonGetAllCats = document.querySelector('[data-getAllCats]');
 const $divContainer = document.querySelector('[data-container]');
 const $buttonAddCat = document.querySelector(`[data-open_modal]`);
 const $modalWindow = document.querySelector(`[data-modal_window]`);
@@ -15,8 +16,8 @@ const cardsHTML = (card)=>{
     <h5 class="card-title">${card.name}</h5>
     <p class="card-text">${card.description}</p>
   </div>
-  <button data-action="show" type="button" class="btn btn-primary">Primary</button>
-  <button data-action="delete" type="button" class="btn btn-danger">Danger</button>
+  <button data-action="show" type="button" class="btn btn-primary">Show</button>
+  <button data-action="delete" type="button" class="btn btn-danger">Delete</button>
 </div>`;  
 }
 
@@ -46,14 +47,36 @@ async function addCat(data){
   })
 }
 
-$buttonGetAllCats.addEventListener("click", getAllCats);
+async function showCatCard(id){
+// console.log(document.forms.add_cat.name);
+  // $modalWindow.classList.remove('hidden');
+  fetch(`${getOneCat}${id}`)
+  .then((response)=>response.json())
+  .then((json)=>{
+    // console.log(json.data.name);
+    console.log(document.forms.add_cat);
+    document.forms.add_cat.id.placeholder = json.data.id;
+    document.forms.add_cat.age.placeholder = json.data.age ?? 0;
+    document.forms.add_cat.catName.placeholder = json.data.name;
+    document.forms.add_cat.rate.placeholder = json.data.rate ?? 0;
+    document.forms.add_cat.description.value = json.data.description;
+    document.forms.add_cat.favourite.placeholder = json.data.favourite;
+    document.forms.add_cat.link.placeholder = json.data.img_link;
+    // console.log(document.forms.add_cat.catName);
+    // console.log();
+  });
+  $modalWindow.classList.remove('hidden');
+}
+
+
 
 $divContainer.addEventListener("click",(e)=>{
-  if (e.target.dataset.action === "show"){}
+  if (e.target.dataset.action === "show"){
+    showCatCard(e.target.closest(`[data-card_id]`).dataset.card_id);
+  }
   else if (e.target.dataset.action === "delete"){
     deleteCard(e.target.closest(`[data-card_id]`).dataset.card_id);
     e.target.closest(`[data-card_id]`).remove();
-    // console.log(e.target.closest(`[data-card_id]`).dataset.card_id);
   }
 });
 
@@ -67,8 +90,16 @@ document.forms.add_cat.addEventListener('submit',(e)=>{
   $divContainer.insertAdjacentHTML('beforeend',cardsHTML(data));
   $modalWindow.classList.add('hidden');
   e.target.reset();
-})
+});
 
 $buttonAddCat.addEventListener('click',(e)=>{
   $modalWindow.classList.remove(`hidden`);
-})
+});
+
+document.addEventListener('keydown',(e)=>{  
+  if (e.key === `Escape`){    
+    $modalWindow.classList.add(`hidden`);
+  }
+});
+
+getAllCats();
